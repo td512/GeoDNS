13196fc21b8b40f78066c5e03c4fee6d7851af09
Theo Morra <theo.morra@chase.net.nz>
2023-07-23 21:04:14 +1200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require File.join(Rails.root, 'lib', 'geoip.rb')
+require 'io/console'
+$stdout.clear_screen
 puts "Checking for #{File.join(Rails.root, 'geoip', 'GeoIP2-City.mmdb')}"
 exit 1 unless File.exist?(File.join(Rails.root, 'geoip', 'GeoIP2-City.mmdb'))
 puts "Checking for #{File.join(Rails.root, 'geoip', 'GeoIP2-ISP.mmdb')}"
 exit 1 unless File.exist?(File.join(Rails.root, 'geoip', 'GeoIP2-ISP.mmdb'))
 puts "Checking for #{File.join(Rails.root, 'geoip', 'GeoIP2-Connection-Type.mmdb')}"
 exit 1 unless File.exist?(File.join(Rails.root, 'geoip', 'GeoIP2-Connection-Type.mmdb'))
-puts "Warning! Warning! Warning! Warning! Warning! Warning! Warning! Warning!"
-puts "(WARN) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-puts "(WARN) This process will take a *very* long time to complete."
+puts "Warning! Warning! Warning! Warning! Warning! Warning! Warning! Warning! Warning!"
+puts "(WARN) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+puts "(WARN) This process will take a *very* long time and a *lot* of RAM to complete."
+puts "(WARN) at approx 300M per CPU core LESS 2. For a CPU with 8 cores, expect at least"
+puts "(WARN) 2.4GB RAM use per /16 imported (using 6 threads) and up to 2GB while"
+puts "(WARN) writing to the database."
 puts "(WARN) If you do not have the time to wait you should hit CTRL+C NOW"
-puts "(WARN) Waiting 5 seconds before beginning import of GeoIP2 Databases"
-puts "(WARN) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-puts "Warning! Warning! Warning! Warning! Warning! Warning! Warning! Warning!"
-sleep 5
+puts "(WARN) Waiting 10 seconds before beginning import of GeoIP2 Databases"
+puts "(WARN) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+puts "Warning! Warning! Warning! Warning! Warning! Warning! Warning! Warning! Warning!"
+sleep 10
 puts "(INFO) Starting to create records in the database. This process WILL"
 puts "(INFO) a very long time. Please be patient, even if the seed appears"
 puts "(INFO) to be frozen."
 (0..255).each do |network|
-    start_ip = "#{network}.0.0.0"
-    end_ip = "#{network}.255.255.255"
-    system("DISABLE_SPRING=true rails runner #{File.join(Rails.root, "runners", "import_maxmind.rb")} #{start_ip} #{end_ip}", out: STDOUT)
+    system("DISABLE_SPRING=true rails runner #{File.join(Rails.root, "runners", "import_maxmind.rb")} #{network}", out: STDOUT)
 end
+puts "(INFO) Seed complete! You can now start the server."
--- runners/import_maxmind.rb
+++ runners/import_maxmind.rb
@@ -1,4 +1,59 @@
 require 'ipaddr'
+require 'concurrent'
+require 'yaml'
+require 'parallel'
+require 'fileutils'
+require 'progressbar'
+require 'io/console'
+
+schedulable_processors = Concurrent.processor_count - 2
+FileUtils.rm_rf(File.join(Rails.root, 'tmp', 'seed-root'))
+FileUtils.mkdir_p(File.join(Rails.root, 'tmp', 'seed-root'))
+
+def import_records(network, subnet)
+  start_ip = IPAddr.new("#{network}.#{subnet}.0.0")
+  end_ip = IPAddr.new("#{network}.#{subnet}.255.255")
+  rows = []
+  (start_ip..end_ip).each do |ip|
+    ip = ip.to_s
+    begin
+      ip_city = @city.city(ip)
+      ip_isp = @isp.isp(ip)
+      ip_connection_type = @connection_type.connection_type(ip)
+      rows.push({
+                  ip:,
+                  continent: ip_city&.continent&.code,
+                  country: ip_city&.country&.name,
+                  country_code: ip_city&.country&.iso_code,
+                  state: ip_city&.most_specific_subdivision&.name,
+                  state_code: ip_city&.most_specific_subdivision&.iso_code,
+                  city: ip_city&.city&.name,
+                  postal_code: ip_city&.postal.code,
+                  latitude: ip_city&.location&.latitude,
+                  longitude: ip_city&.location&.longitude,
+                  autonomous_system_number: ip_isp&.autonomous_system_number,
+                  autonomous_system_organization: ip_isp&.autonomous_system_organization,
+                  isp: ip_isp.isp,
+                  organization: ip_isp.organization,
+                  connection_type: ip_connection_type.connection_type
+                })
+      puts "(INFO) [#{ip_isp&.autonomous_system_organization}] #{ip} Successfully imported from MaxMind Database. Locality is #{ip_city&.city&.name}, #{ip_city&.country&.name}, Connection Type: #{ip_connection_type.connection_type}"
+    rescue MaxMind::GeoIP2::AddressNotFoundError
+      puts "(INFO) Address #{ip} wasn't found in the MaxMind Database. Inserting as bogon."
+      rows.push({
+                  ip:,
+                  country: 'BOGON',
+                  country_code: 'ZZ',
+                  connection_type: 'BOGON'
+                })
+    end
+    ip_isp = nil
+    ip_city = nil
+    ip_connection_type = nil
+    ip = nil
+  end
+  File.open((File.join(Rails.root, 'tmp', 'seed-root', "#{network}.#{subnet}.0.0.yaml")), 'w') { |f| f.write(rows.to_yaml) }
+end
 
 @city = MaxMind::GeoIP2::Reader.new(
   database: File.join(Rails.root, 'geoip', 'GeoIP2-City.mmdb')
@@ -9,40 +64,32 @@ require 'ipaddr'
 @connection_type = MaxMind::GeoIP2::Reader.new(
   database: File.join(Rails.root, 'geoip', 'GeoIP2-Connection-Type.mmdb')
 )
-start_ip = IPAddr.new(ARGV[0])
-end_ip = IPAddr.new(ARGV[1])
-(start_ip..end_ip).each do |ip|
-  ip = ip.to_s
-  unless GeoIp.find_by(ip: ip)
-    begin
-      ip_city = @city.city(ip)
-      ip_isp = @isp.isp(ip)
-      ip_connection_type = @connection_type.connection_type(ip)
-      GeoIp.create!(
-        ip:,
-        continent: ip_city.continent.code,
-        country: ip_city.country.name,
-        country_code: ip_city.country.iso_code,
-        state: ip_city.most_specific_subdivision.name,
-        state_code: ip_city.most_specific_subdivision.iso_code,
-        postal_code: ip_city.postal.code,
-        latitude: ip_city.location.latitude,
-        longitude: ip_city.location.longitude,
-        autonomous_system_number: ip_isp.autonomous_system_number,
-        autonomous_system_organization: ip_isp.autonomous_system_organization,
-        isp: ip_isp.isp,
-        organization: ip_isp.organization,
-        connection_type: ip_connection_type.connection_type
-      )
-      puts "(INFO) [#{ip_isp.autonomous_system_organization}] #{ip} Successfully imported from MaxMind Database. Country is #{ip_city.country.name}. Connection Type: #{ip_connection_type.connection_type}"
-    rescue MaxMind::GeoIP2::AddressNotFoundError
-      puts "(INFO) Address #{ip} wasn't found in the MaxMind Database."
-    end
-    ip_isp = nil
-    ip_city = nil
-    ip_connection_type = nil
-    ip = nil
-  else
-    puts "(INFO) We've already imported this IP. Moving on."
-  end
+
+network = ARGV[0]
+counter = 1
+rows = []
+
+puts "(INFO) Spawning #{schedulable_processors} threads"
+Parallel.each((0..255), in_processes: schedulable_processors) do |subnet|
+  import_records(network, subnet)
 end
+$stdout.clear_screen
+puts "(INFO) Importing records into database. This may take a while. Please be patient."
+progressbar = ProgressBar.create(format: '%a |%b>%i| %p%% (%c/%C) %E',
+                   title: "Records",
+                   total: 1,
+                   starting_at: 0)
+16_777_215.times { progressbar.total += 1 }
+(0..255).each do |subnet|
+  rows += YAML.load(
+    File.read(
+      File.join(
+        Rails.root, 'tmp', 'seed-root', "#{network}.#{subnet}.0.0.yaml" )
+      )
+  )
+  GeoIp.insert_all!(rows)
+  65_536.times { progressbar.increment }
+  rows.clear
+end
+FileUtils.rm_rf(File.join(Rails.root, 'tmp', 'seed-root'))
+exit 0
